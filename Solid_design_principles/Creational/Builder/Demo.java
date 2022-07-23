9006203f12c292cbabc3a9170e05cd97708005cc
NikhilSharma-NS <nkhlsharma050@gmail.com>
2022-07-23 10:46:37 +0530

    feat: builder and factory design

---
+++ Solid_design_principles/Creational/Builder/Demo.java
@@ -0,0 +1,15 @@
+package com.builder;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        String[] word={"hello","world"};
+        StringBuilder stringBuilder=new StringBuilder();
+        for (String w: word)
+        {
+            stringBuilder.append(w);
+        }
+
+        System.out.println(stringBuilder);
+    }
+}
---
+++ Solid_design_principles/Creational/Builder/README.md
@@ -0,0 +1,9 @@
+#### Builder 
+
+This provides an API for constructing a n object step by step
+
+Fluent Builder
+
+Faceted Builder
+
+
